37815d9c992b5af9e0eeeeb4c56928956ea7efbc
vatrasar <vatrasar@gmail.com>
2018-12-23 10:07:26 +0100

    views before and after works

--- main.py
+++ main.py
@@ -2,34 +2,59 @@ from templatka import Ui_MainWindow
 from PyQt5 import  QtWidgets,QtCore
 import sys
 from PyQt5.QtWidgets import QApplication,QDialog,QLabel
+from scipy.signal import freqz
+from scipy.signal import butter, lfilter
+import numpy as np
+import pyqtgraph as pg
+
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return b, a
 
 
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
 
 class Gui:
     def __init__(self,window):
+
+        self.compute_plots()
+        plot1=self.compute_plots()
+        amplitude_before,amplitude_after,time=self.compute_plots()
         self.ui = Ui_MainWindow()
-        self.ui.setupUi(window)
+        self.ui.setupUi(window,amplitude_before,amplitude_after,time)
 
 
+    def compute_plots(self):
+        # Sample rate and desired cutoff frequencies (in Hz).
+        fs = 5000.0
+        T = 0.05
+        lowcut = 500.0
+        highcut = 1250.0
+        nsamples = T * fs
+        time = np.linspace(0, T, nsamples, endpoint=False)
+        amplitude_before = 0.1 * np.sin(2 * np.pi * 1.2 * np.sqrt(time))
+        amplitude_before += 0.01 * np.cos(2 * np.pi * 312 * time + 0.1)
+        amplitude_before += 0.02 * np.cos(2 * np.pi * 600.0 * time + .11)
+        amplitude_before += 0.03 * np.cos(2 * np.pi * 2000 * time)
+
+        amplitude_after = butter_bandpass_filter(amplitude_before, lowcut, highcut, fs, order=6)
+
+        return amplitude_before,amplitude_after,time
 
 
 
 
 if __name__ == "__main__":
-    try:
 
-
-        app = QApplication(sys.argv)
-        window=QtWidgets.QMainWindow()
-        gui=Gui(window)
-        b=gui.ui.menubar.actions()
-        window.show()
-        sys.exit(app.exec_())
-    except ValueError:
-        dialog = QDialog()
-        ok_button = QLabel("Nie można wczytać tego pliku.\n Proszę wybrać inny.", dialog)
-        # ok_button.move(80,50)
-        dialog.resize(200, 80)
-        dialog.setWindowModality(QtCore.Qt.ApplicationModal)
-        dialog.setWindowTitle("Błąd")
-        dialog.exec_()
+    app = QApplication(sys.argv)
+    window=QtWidgets.QMainWindow()
+    gui=Gui(window)
+    b=gui.ui.menubar.actions()
+    window.show()
+    sys.exit(app.exec_())
--- templatka.py
+++ templatka.py
@@ -7,24 +7,30 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+import pyqtgraph as pg
+import numpy as np
 
 class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
+
+
+
+    def setupUi(self, MainWindow,amplitude_before,amplitude_after,time):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(771, 600)
+        self.make_plots(amplitude_before,amplitude_after,time)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
         self.verticalLayout.setObjectName("verticalLayout")
-        self.widok_przed = QtWidgets.QWidget(self.centralwidget)
+        self.widok_przed = self.plot
         self.widok_przed.setObjectName("widok_przed")
         self.verticalLayout.addWidget(self.widok_przed)
-        self.widok_po = QtWidgets.QWidget(self.centralwidget)
+        self.widok_po = self.plot2
         self.widok_po.setObjectName("widok_po")
         self.verticalLayout.addWidget(self.widok_po)
-        self.widget = QtWidgets.QWidget(self.centralwidget)
-        self.widget.setObjectName("widget")
-        self.verticalLayout.addWidget(self.widget)
+        self.chara_amplitudowa = QtWidgets.QWidget(self.centralwidget)
+        self.chara_amplitudowa.setObjectName("chara_amplitudowa ")
+        self.verticalLayout.addWidget(self.chara_amplitudowa )
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 771, 22))
@@ -49,4 +55,6 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.menuEdycja.setTitle(_translate("MainWindow", "Edycja"))
         self.actionzmie_order.setText(_translate("MainWindow", "zmień order"))
-
+    def make_plots(self,amplitude_before,amplitude_after,time):
+        self.plot = pg.PlotWidget(y=amplitude_before, x=time)
+        self.plot2 = pg.PlotWidget(y=amplitude_after, x=time)
